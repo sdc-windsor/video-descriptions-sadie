19f1d9fb19fa17c5b2e9d36f2a48005c439a1ad2
Huy Luu <lmhuy_lmhuy@yahoo.com>
2019-02-18 22:37:31 -0800

    refactor find() method per Chase suggestion

--- server/index.js
+++ server/index.js
@@ -15,7 +15,11 @@ app.get('/categories/:video_id', function (req, res) {
 app.get('/videosByCategory/:category', function (req, res) {
     // console.log(req.params.category)
     Description.find(
-            {categories: {$in:  [req.params.category] }}
+            {
+                categories: {
+                    $in:  [req.params.category]
+                }
+            }
         ).then((data)=>{
         res.json(data);
         res.end();
