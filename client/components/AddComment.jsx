62501903970539bb4aa7c0af8d95073ee94f591e
Jason Bourget <jason.andre.bourget@gmail.com>
2019-03-14 22:47:47 -0600

    Fixed frontend code

--- client/components/AddComment.jsx
+++ client/components/AddComment.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import axios from 'axios';
-import {userThumbNail} from '../styles';
+import { userThumbNail } from '../styles';
 import { Button } from 'reactstrap';
 
-class AddComment extends React.Component{
+class AddComment extends React.Component {
     constructor(props) {
         super(props);
-        this.state={
+        this.state = {
             comment: 'Comment',
             info: ''
         }
     }
 
     getUserInfo(user_id) {
-        axios.get(`http://localhost:8081/usersthumbnail/${user_id}`).then((data)=>{
+        axios.get(`http://localhost:8081/usersthumbnail/${user_id}`).then((data) => {
             this.setState({
                 info: data.data
             })
@@ -21,24 +21,24 @@ class AddComment extends React.Component{
     }
 
     componentDidMount() {
-        this.getUserInfo('5c7af250b5479233947f45c2');
+        this.getUserInfo('5c8b1feba0a0f7484fb970b2');
     }
 
     render() {
         return (
-            <div style={{  borderTop: '0.05em solid #e8eaed', borderBottom: '0.05em solid #e8eaed', paddingTop: '1em', paddingBottom: '1em', display: 'flex'}}>
+            <div style={{ borderTop: '0.05em solid #e8eaed', borderBottom: '0.05em solid #e8eaed', paddingTop: '1em', paddingBottom: '1em', display: 'flex' }}>
                 <div>
                     <img src={this.state.info.user_thumbnail} style={userThumbNail} />
                 </div>
-                <div style={{paddingLeft:'2em', flex: 'auto'}}>
+                <div style={{ paddingLeft: '2em', flex: 'auto' }}>
                     <div>Add a new Comment</div>
-                    <textarea style={{width: '100%', height:'5em'}} value={this.props.comment} onChange={(event)=>{this.props.updateInput(event.target.value)}}></textarea>
-                    <div><Button color="info" style={{padding: "5px"}}
-                                 onClick={()=>{
-                                    let id = window.location.pathname;
-                                    id = id.split('/');
-                                    this.props.sendComment(Number(id[1]));
-                                    }}
+                    <textarea style={{ width: '100%', height: '5em' }} value={this.props.comment} onChange={(event) => { this.props.updateInput(event.target.value) }}></textarea>
+                    <div><Button color="info" style={{ padding: "5px" }}
+                        onClick={() => {
+                            let id = window.location.pathname;
+                            id = id.split('/');
+                            this.props.sendComment(Number(id[1]));
+                        }}
                     >Add Comment</Button>{' '}</div>
                 </div>
             </div>
