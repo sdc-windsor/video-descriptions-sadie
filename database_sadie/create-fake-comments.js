f08d22b17e4f7784bc9666f8f4fcf0a6591f6c3f
Sadie Gill <thesadie@gmail.com>
2019-03-21 21:19:11 -0700

    create fake comments script

--- database_sadie/create-fake-comments.js
+++ database_sadie/create-fake-comments.js
@@ -1,6 +1,7 @@
 const faker = require('faker');
+const fs = require('fs');
 const _ = require('underscore');
-const { writeBatch, getRandomInt } = require('./fake-data-helpers.js');
+const { getRandomInt } = require('./fake-data-helpers.js');
 
 const getRandomCommentQuantity = () => {
   return (Math.random() > .9) ? getRandomInt(20) : getRandomInt(5);
@@ -10,20 +11,42 @@ const makeComment = (id, vid_id) => {
   let _id = id;
   let video_id = vid_id;
   let user_id = getRandomInt(29000);
-  let comment = faker.sentence();
+  let comment = faker.lorem.sentence();
 
-  return [_id, video_id, user_id, comment];
+  return [_id, video_id, user_id, comment].join(', ');
 };
+
+async function writeBatchComments(ids, fileName, comment_id) {
+  let dataString = ids.map((id) => {
+    let numOfComments = getRandomCommentQuantity();
+    var comments = [];
+    while (numOfComments > 0) {
+      comments.push(makeComment(comment_id, id));
+      numOfComments--;
+      comment_id++;
+    }
+    return comments.join('\r');
+  }).join('\r')
+
+
+  await fs.writeFile(__dirname + fileName, dataString, (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+  });
+  return comment_id;
+
+}
+
 
 async function createFakeComments() {
   let start = 1;
   let end = 1000001;
+  var comment_id = 1;
   for (let i = 0; i < 10; i++) {
-    let ids = await _.range(start, end);
-    await writeBatch(ids, `/batch_${i + 1}_comments.txt`, makeComment)
-    result.push(ids);
-    start += 100000;
-    end += 100000;
+    let ids = _.range(start, end);
+    comment_id = await writeBatchComments(ids, `/batch_${i + 1}_comments.txt`, comment_id);
+    start += 1000000;
+    end += 1000000;
   }
 };
 
