d0849d07299859926870d8452e59f8bca9e69fef
Sadie Gill <thesadie@gmail.com>
2019-03-26 15:26:13 -0700

    in progress redoing get requests

--- database/index.js
+++ database/index.js
@@ -1,53 +1,14 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/fec', { useNewUrlParser: true });
+const { Pool, Client } = require('pg');
+const config = require('./../config.js');
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-    console.log('Connected to db!')
+const pool = new Pool({
+  user: config.user,
+  database: 'vimeo',
+  password: config.password,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
 });
 
-const descriptionSchema = new mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    video_id: {
-        type: Number,
-        unique: true,
-        dropDups: true
-    },
-    description: String,
-    categories: [String],
-    likes: Number
-});
+module.exports = pool;
 
-const Description = mongoose.model('Description', descriptionSchema);
-
-const commentSchema = new mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    video_id: Number,
-    user_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    },
-    comment: String,
-    date: Date
-});
-
-const Comment = mongoose.model('Comment', commentSchema);
-
-const userSchema = new mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    username:{
-        type: String,
-        unique: true,
-        dropDups: true
-    },
-    user_thumbnail: String
-});
-
-const User = mongoose.model('User', userSchema);
-
-module.exports = {
-    Description,
-    Comment,
-    User
-}
--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-const Description = require('../database/index').Description;
-const User = require('../database/index').User;
-const Comment = require('../database/index').Comment;
+const pool = require('../database/index.js');
+// const Description = require('../database/index').Description;
+// const User = require('../database/index').User;
+// const Comment = require('../database/index').Comment;
 const saveComment = require('../database/helper').saveComment;
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const path = require('path');
 
 app.use(bodyParser.json());
 
@@ -16,66 +16,80 @@ app.use(express.static('public'));
 app.use('/:id', express.static('public'));
 
 app.get('/categories/:video_id', function (req, res) {
-    Description.findOne({video_id: req.params.video_id}).then((data)=>{
-        res.json(data);
-        res.end();
-    })
+	pool.query('SELECT * FROM descriptions WHERE video_id = $1', [req.params.video_id])
+		.then(data => {
+			res.json(data.rows[0]);
+			res.end();
+		})
+		.catch(e => console.log(e))
 });
 
 app.get('/usersthumbnail/:user_id', function (req, res) {
-    User.findOne({_id: req.params.user_id}).then((data)=>{
-        res.json(data);
-        res.end();
-    })
+	pool.query('SELECT * FROM users WHERE _id = $1', [req.params.user_id])
+		.then(data => {
+			res.json(data.rows[0]);
+			res.end();
+		})
+		.catch(e => console.log(e))
 });
 
-app.get('/userid/:username', function(req,res){
-    User.findOne({username: req.params.username}).then((data)=>{
-        res.json(data._id);
-        res.end();
-    })
+app.get('/userid/:username', function (req, res) {
+	pool.query('SELECT * FROM users WHERE username = $1', [req.params.username])
+		.then(data => {
+			res.json(data.rows[0]);
+			res.end();
+		})
+		.catch(e => console.log(e))
+
 })
 
 app.get('/videosByCategory/:category', function (req, res) {
-    const arrayOfCategories = [];
-    const splitParams = req.params.category.split(',');
-    for (let l = 0 ; l< splitParams.length;l++) {
-        arrayOfCategories.push({
-            categories: {
-                // "/mon/i"
-                $regex : new RegExp(`${splitParams[l]}`,'i')
-            }
-        })
-    }
+	const arrayOfCategories = [];
+	const splitParams = req.params.category.split(',');
+	for (let l = 0; l < splitParams.length; l++) {
+		arrayOfCategories.push({
+			categories: {
+				// "/mon/i"
+				$regex: new RegExp(`${splitParams[l]}`, 'i')
+			}
+		})
+	}
 
-    Description.find({$and: arrayOfCategories})
-               .then((data)=>{
-                    res.json(data);
-                    res.end();
-                });
+	Description.find({ $and: arrayOfCategories })
+		.then((data) => {
+			res.json(data);
+			res.end();
+		});
+
+	pool.query('SELECT * FROM descriptions WHERE $1 = ANY (categories);', [req.params.categories])
 });
 
 app.get('/comments/:video_id', function (req, res) {
-    Comment.find({video_id: req.params.video_id}).sort({data:-1}).exec().then((data)=>{
-        res.json(data);
-        res.end();
-    })
+	pool.query('SELECT * FROM comments WHERE video_id = $1', [req.params.video_id])
+	.then(data => {
+		res.json(data.rows[0]);
+		res.end();
+	})
+	.catch(e => console.log(e))
 });
 
 app.get('/details/:video_id', function (req, res) {
-    Description.find({video_id: req.params.video_id}).then((data)=>{
-        res.json(data);
-        res.end();
-    })
+	pool.query('SELECT * FROM descriptions WHERE video_id = $1', [req.params.video_id])
+	.then(data => {
+		res.json(data.rows[0]);
+		res.end();
+	})
+	.catch(e => console.log(e))
 });
 
 app.post('/comments/:video_id', function (req, res) {
-    console.log('REQ BODY', req.body);
-    saveComment(req.body.video_id, req.body.user_id, req.body.comment, req.body.date, ()=>{
-        console.log('Saved comment to database')
-        res.send('Saved comment to database');
-        res.end();
-    })
+	console.log('REQ BODY', req.body);
+	saveComment(req.body.video_id, req.body.user_id, req.body.comment, req.body.date, () => {
+		console.log('Saved comment to database')
+		res.send('Saved comment to database');
+		res.end();
+	})
+
 });
 
 /// need full CRUD for description and comments
