9eb9790d2b46157d850820e965e087b36c656170
Sadie Gill <thesadie@gmail.com>
2019-04-23 22:00:57 -0700

    add gzip compression to webpack and s3 uploaded bundle url

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
     entry: ['@babel/polyfill', __dirname + '/client/index.jsx'],
@@ -7,6 +8,12 @@ module.exports = {
       path: path.resolve(__dirname, 'public')
     },
     mode: 'production',
+    plugins: [
+      new CompressionPlugin({
+        algorithm: 'gzip',
+        test: /\.(js|css|html|svg)$/
+      })
+    ],
     module: {
       rules: [
         {
